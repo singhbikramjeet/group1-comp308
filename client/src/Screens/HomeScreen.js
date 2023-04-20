fc2acdcc068d4050e1a1c25fd2b7a51e54ffbc61
Bikramjeet Singh <b102@my.centennialcollege.ca>
2023-04-20 19:01:15 -0400

    closes #2

--- client/src/Screens/HomeScreen.js
+++ client/src/Screens/HomeScreen.js
@@ -8,9 +8,9 @@ function HomeScreen() {
         and attention you need to get back on your feet. We understand that being in the hospital can be a difficult 
         and stressful experience, which is why we strive to create a welcoming and comfortable environment for all of our 
         patients. Thank you for choosing our hospital for your healthcare needs. We look forward to serving you and helping 
-        you on your road to recovery.</p>
+        you on your road to recovery. </p>
         <div className="d-flex justify-content-center align-items-center">
-        <img src="https://media.istockphoto.com/id/1207314090/photo/child-holding-yellow-balloon-in-the-hands.jpg?s=612x612&w=0&k=20&c=XIka5vPP8AOvNslEHHGV5rxRrHXMg2HVCaqzw8Fs0iU=" alt="Hospital" className="img-fluid" />
+        <img src="https://media.istockphoto.com/id/1207314090/photo/child-holding-yellow-balloon-in-the-hands.jpg?s=612x612&w=0&k=20&c=XIka5vPP8AOvNslEHHGV5rxRrHXMg2HVCaqzw8Fs0iU=" alt="Hospital" className="img-fluid"/>
       </div>
     </div>
   )
